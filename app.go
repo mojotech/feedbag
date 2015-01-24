f550da34a83f6ca605b72fb3fec7c70958cf33c1
Brian Egizi <brian@mojotech.com>
2015-01-23 22:40:33 -0500

    Build out gorp support for sqlite
    Add a simple User model

--- app.go
+++ app.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"feedbag/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"feedbag/Godeps/_workspace/src/github.com/go-gorp/gorp"
 	"feedbag/Godeps/_workspace/src/github.com/markbates/goth"
 	"feedbag/Godeps/_workspace/src/github.com/markbates/goth/providers/github"
+	_ "feedbag/Godeps/_workspace/src/github.com/mattn/go-sqlite3"
 )
 
 var (
 	configPort   = flag.String("port", "3000", "Port to run the server on")
 	templatesDir = flag.String("templates", "./templates", "Path to your templates directory")
+	dbmap        = setupDb()
 )
 
 func main() {
 	//Setup gin
 	r := gin.Default()
 	setupRoutes(r)
+
+	// Close the database connection if we fail
+	defer dbmap.Db.Close()
 
 	// Setup Goth Authentication
 	goth.UseProviders(
@@ -33,4 +41,33 @@ func main() {
 
 	// Listen and Serve on port from ENV or flag
 	r.Run(fmt.Sprintf(":%s", port))
+}
+
+func setupDb() *gorp.DbMap {
+	// connect to db using standard Go database/sql API
+	// use whatever database/sql driver you wish
+	db, err := sql.Open("sqlite3", "./tmp/feedbag.bin")
+	checkErr(err, "sql.Open failed")
+
+	// construct a gorp DbMap
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.SqliteDialect{}}
+
+	// add a table, setting the table name to 'posts' and
+	// specifying that the Id property is an auto incrementing PK
+	dbmap.AddTableWithName(User{}, "users").SetKeys(true, "Id")
+
+	// create the table. in a production system you'd generally
+	// use a migration tool, or create the tables via scripts
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err, "Create tables failed")
+
+	return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+
+	}
+
 }
---
+++ models.go
@@ -0,0 +1,6 @@
+package main
+
+type User struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+}
