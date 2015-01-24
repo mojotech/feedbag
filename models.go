26911213d037da25bc97784a232d9bdb3477b64f
Brian Egizi <brian@mojotech.com>
2015-01-23 22:52:52 -0500

    Enhance User model

--- models.go
+++ models.go
@@ -1,6 +1,13 @@
 package main
 
 type User struct {
-	Id   int64  `json:"id"`
-	Name string `json:"name"`
+	Id          int64       `json:"id"`
+	Name        string      `json:"name"`
+	Username    string      `json:"username"`
+	AvatarUrl   string      `json:"avatar_url"`
+	ProfileUrl  string      `json:"profile_url"`
+	Email       string      `json:"email"`
+	Joined      string      `json:"joined"`
+	Raw         interface{} `json:"raw"`
+	AccessToken string      `json:"-"`
 }
