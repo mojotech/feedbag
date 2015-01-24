b83be56fcce89f25e46d38b8f212e57328763f1e
Brian Egizi <brian@mojotech.com>
2015-01-23 21:50:09 -0500

    Add Goth github user authentication

--- app.go
+++ app.go
@@ -5,11 +5,19 @@ import (
 	"os"
 
 	"feedbag/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"feedbag/Godeps/_workspace/src/github.com/markbates/goth"
+	"feedbag/Godeps/_workspace/src/github.com/markbates/goth/providers/github"
 )
 
 func main() {
+	//Setup gin
 	r := gin.Default()
 	setupRoutes(r)
+
+	// Setup Goth Authentication
+	goth.UseProviders(
+		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
+	)
 
 	port := os.Getenv("PORT")
 
--- routes.go
+++ routes.go
@@ -1,12 +1,47 @@
 package main
 
-import "feedbag/Godeps/_workspace/src/github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"feedbag/Godeps/_workspace/src/github.com/gin-gonic/gin"
+	"feedbag/Godeps/_workspace/src/github.com/markbates/goth/gothic"
+)
 
 func setupRoutes(r *gin.Engine) {
+	//Oauth Authenticaton and Callbacks
+	r.GET("/auth/github/callback", providerCallback)
+	r.GET("/auth/github", providerAuth)
+
+	//Api endpoints
 	a := r.Group("api")
 	a.GET("/activity", getActivity)
 }
 
 func getActivity(c *gin.Context) {
 	c.JSON(200, gin.H{"activity": "here"})
+}
+
+func providerCallback(c *gin.Context) {
+	// print our state string to the console
+	fmt.Println(c.Request.URL.Query().Get("state"))
+
+	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
+	if err != nil {
+		fmt.Fprintln(c.Writer, err)
+		return
+
+	}
+
+	c.JSON(200, gin.H{"user": user})
+
+}
+
+func providerAuth(c *gin.Context) {
+	gothic.GetProviderName = getProviderName
+	gothic.BeginAuthHandler(c.Writer, c.Request)
+}
+
+func getProviderName(req *http.Request) (string, error) {
+	return "github", nil
 }
