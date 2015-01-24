70423d487e319bd7081f7395b98743ae49c0e8ff
Brian Egizi <brian@mojotech.com>
2015-01-23 22:00:21 -0500

    Switch to flags for port and template directory

--- app.go
+++ app.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"feedbag/Godeps/_workspace/src/github.com/gin-gonic/gin"
 	"feedbag/Godeps/_workspace/src/github.com/markbates/goth"
 	"feedbag/Godeps/_workspace/src/github.com/markbates/goth/providers/github"
+)
+
+var (
+	configPort   = flag.String("port", "3000", "Port to run the server on")
+	templatesDir = flag.String("templates", "./templates", "Path to your templates directory")
 )
 
 func main() {
@@ -19,8 +25,12 @@ func main() {
 		github.New(os.Getenv("GITHUB_KEY"), os.Getenv("GITHUB_SECRET"), "http://localhost:3000/auth/github/callback"),
 	)
 
-	port := os.Getenv("PORT")
+	port := *configPort
 
-	// Listen and Server in 0.0.0.0:8080
+	if len(os.Getenv("PORT")) > 0 {
+		port = os.Getenv("PORT")
+	}
+
+	// Listen and Serve on port from ENV or flag
 	r.Run(fmt.Sprintf(":%s", port))
 }
