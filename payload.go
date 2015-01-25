5392154ec6a00c9dfe87b7a8d2c6023a2da60eb2
Brian Egizi <brian@mojotech.com>
2015-01-25 10:41:02 -0500

    Add more items to the payload processor

--- payload.go
+++ payload.go
@@ -8,13 +8,21 @@ import (
 
 type ActivityPayload struct {
 	EventType string `json:"event_type"`
-	//All possible variables/events/etc
-	OpenPullRequest    bool   `json:"open_pull_request"`
-	ClosePullRequest   bool   `json:"close_pull_request"`
-	CreateIssueComment bool   `json:"create_issue_comment"`
-	PRTitle            string `json:"pr_title"`
-	PRNumber           int    `json:"pr_number"`
-	IssueNumber        int    `json:"issue_number"`
+
+	//Events
+	OpenPullRequest    bool `json:"open_pull_request"`
+	ClosePullRequest   bool `json:"close_pull_request"`
+	OpenIssue          bool `json:"open_issue"`
+	CreateIssueComment bool `json:"create_issue_comment"`
+
+	//Variables
+	Title     string `json:"title"`
+	Body      string `json:"body"`
+	Number    int    `json:"number"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	ClosedAt  string `json:"closed_at"`
+	MergedAt  string `json:"merged_at"`
 }
 
 func ProcessPayload(e []github.Event, u User) ([]ActivityPayload, error) {
@@ -40,12 +48,60 @@ func ProcessPayload(e []github.Event, u User) ([]ActivityPayload, error) {
 			}
 
 			if pr, ok := rawPayload["pull_request"].(map[string]interface{}); ok {
+				if val, ok := pr["title"].(string); ok {
+					payload.Title = val
+				}
+				if val, ok := pr["body"].(string); ok {
+					payload.Body = val
+				}
+				if val, ok := pr["number"].(float64); ok {
+					payload.Number = int(val)
+				}
 
-				// Set title of PR
-				payload.PRTitle = pr["title"].(string)
+				//Set times
+				if val, ok := pr["created_at"].(string); ok {
+					payload.CreatedAt = val
+				}
+				if val, ok := pr["updated_at"].(string); ok {
+					payload.UpdatedAt = val
+				}
+				if val, ok := pr["closed_at"].(string); ok {
+					payload.ClosedAt = val
+				}
+				if val, ok := pr["merged_at"].(string); ok {
+					payload.MergedAt = val
+				}
+			}
+			activityPayloads = append(activityPayloads, payload)
 
-				// Set Pr Number
-				payload.PRNumber = int(pr["number"].(float64))
+		case "IssueEvent":
+			if val, ok := rawPayload["action"]; ok && val == "opened" {
+				payload.EventType = "OpenIssue"
+				payload.OpenIssue = true
+			}
+
+			if issue, ok := rawPayload["issue"].(map[string]interface{}); ok {
+				if val, ok := issue["number"].(float64); ok {
+					payload.Number = int(val)
+				}
+				if val, ok := issue["title"].(string); ok {
+					payload.Title = val
+				}
+				if val, ok := issue["body"].(string); ok {
+					payload.Body = val
+				}
+
+				//Set times
+				if val, ok := issue["created_at"].(string); ok {
+					payload.CreatedAt = val
+				}
+				if val, ok := issue["updated_at"].(string); ok {
+					payload.UpdatedAt = val
+				}
+				if val, ok := issue["closed_at"].(string); ok {
+					payload.ClosedAt = val
+				}
+
 			}
 			activityPayloads = append(activityPayloads, payload)
 
@@ -54,13 +110,20 @@ func ProcessPayload(e []github.Event, u User) ([]ActivityPayload, error) {
 				payload.EventType = "CreateIssueComment"
 				payload.CreateIssueComment = true
 			}
-
 			if issue, ok := rawPayload["issue"].(map[string]interface{}); ok {
-				payload.IssueNumber = int(issue["number"].(float64))
-
+				if val, ok := issue["number"].(float64); ok {
+					payload.Number = int(val)
+				}
+			}
+			if comment, ok := rawPayload["comment"].(map[string]interface{}); ok {
+				if val, ok := comment["title"].(string); ok {
+					payload.Title = val
+				}
+				if val, ok := comment["body"].(string); ok {
+					payload.Body = val
+				}
 			}
 			activityPayloads = append(activityPayloads, payload)
-
 		}
 	}
 
