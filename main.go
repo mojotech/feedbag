257cd769c1cfbda39b0d145e53d14ad08dad4d67
Brian Egizi <brian@mojotech.com>
2015-02-12 16:34:20 -0500

    Serve static assets

--- main.go
+++ main.go
@@ -4,26 +4,54 @@ import (
 	"flag"
 	"log"
 	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/mojotech/feedbag/feedbag"
 )
 
 var (
-	configPort   = flag.String("port", "3000", "Port to run the server on")
-	templatesDir = flag.String("templates", "./templates", "Path to your templates directory")
+	port         string
+	templatesDir string
+	publicDir    string
 )
 
-func main() {
-	//Parse flags
-	flag.Parse()
+func flags() {
+	flag.StringVar(&port, "port", "3000", "Port to run the server on")
+	flag.StringVar(&templatesDir, "templates", "", "Path to your templates directory")
+	flag.StringVar(&publicDir, "public", "", "Path to your public assets folder")
 
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	flag.Parse()
+}
+
+func folders() {
+	_, file, _, _ := runtime.Caller(0)
+	here := filepath.Dir(file)
+
+	if publicDir == "" {
+		publicDir = filepath.Join(here, "/public")
+	}
+
+	if templatesDir == "" {
+		templatesDir = filepath.Join(here, "/templates")
+	}
+}
+
+func init() {
+	flags()
+	folders()
+}
+
+func main() {
 	// Configure port for server to run on
-	port := *configPort
 	if len(os.Getenv("PORT")) > 0 {
 		port = os.Getenv("PORT")
 	}
 
-	err := feedbag.Start(port, *templatesDir)
+	err := feedbag.Start(port, templatesDir, publicDir)
 	if err != nil {
 		log.Fatalln("Feedbag failed to start:", err)
 	}
